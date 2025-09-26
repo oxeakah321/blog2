151b0f8e4feb4f4a8ccf6bcb437b9b2719aee593
oxeakah321 <ktoledopessoa@gmail.com>
2025-09-26 18:54:27 -0300

    fim

---
+++ login_formulario.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login | Projeto para Web com PHP</title>
+        <link rel="stylesheet" 
+              href="lib/bootstrap-4.2.1-dist/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <?php include 'includes/topo.php'; ?>
+                </div>
+            </div>
+            <div class="row" style="min-height: 500px;">
+            <div class="col-md-12">
+                    <?php include 'includes/menu.php'; ?>
+                </div>
+                <div class="col-md-10" style="padding-top: 50px;">
+                    <div class="card-hearder">Login</div>
+                     <div class="card-body">
+                        <form method="post" action="core/usuario_repositorio.php">
+                            <input type="hidden" name="acao" value="login">
+                            <div class="form-group">
+                                <label for="email">E-Mail</label>
+                                <input class="form-control" type="text"
+                                require="required" id="email" name="email">
+                            </div>
+                            <div class="form-group">
+                                <label for="senha">Senha</label>
+                                <input class="form-control" type="password"
+                                require="required" id="senha" name="senha">
+                            </div>
+                     </div>
+                </div>
+            </div>
+                        </form>
+            <div class="text-right">
+                <button class="btn btn-sucess"
+                         type="submit">Acessar</button>
+            </div>
+        </div>
+    </body>
+</html>
