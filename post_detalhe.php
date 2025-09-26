0d6182c259ac5aa8c824f28a67a86f2966e9ddaa
oxeakah321 <ktoledopessoa@gmail.com>
2025-09-26 15:37:09 -0300

    finaliza

--- post_detalhe.php
+++ post_detalhe.php
@@ -1,16 +1,15 @@
-<!DOCTYPE <html>
- <?php 
+<!DOCTYPE html>
+<?php
     require_once 'includes/funcoes.php';
     require_once 'core/conexao_mysql.php';
     require_once 'core/sql.php';
     require_once 'core/mysql.php';
 
-    foreach ($_GET as $indice =>$dado) {
-        $$indice = limparDados ($dado);
-
+    foreach($_GET as $indice => $dado){
+        $$indice = limparDados($dado);
     }
 
-    $posts = buscar (
+    $posts = buscar(
         'post',
         [
             'titulo',
@@ -21,29 +20,55 @@
                 where usuario.id = post.usuario_id) as nome'
         ],
         [
-            ['id', '=', $post] 
+            ['id', '=', $post]
         ]
     );
     $post = $posts[0];
     $data_post = date_create($post['data_postagem']);
-    $data_post = date_format($data_post, 'd/m/Y H:i:s:');
-    ?>  
-    <html>
-        <<head>
-            <title> <?php echo $post ['titulo'] ?></title>
-            <link rel="stylesheet" 
-                  href="lib/bootstrap-4.2.1-dist/css/bootstrap.min.css">
-        </head>
-        <body>
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <?php 
-                            include 'includes/rodape.php';
-                        ?>
+    $data_post = date_format($data_post, 'd/m/Y H:i:s');
+
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $post['titulo']?></title>
+    <link rel="stylesheet" href="lib/bootstrap-4.2.1-dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <?php
+                    include 'includes/topo.php';
+                ?>
+
+            </div>
+        </div>
+        <div class="row" style="min-height: 500px;">
+            <div class="col-md-12">
+                <?php include 'includes/menu.php';?>
+            </div>
+            <div class="col-md-10" style="padding-top: 50px;">
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo $post['titulo'] ?> </h5>
+                    <h5 class="card-subtitle mb-2 text-muted"> 
+                        <?php echo $data_post ?> Por <?php echo $post['nome'] ?>
+                    </h5>
+                    <div class="card-text">
+                        <?php echo html_entity_decode($post['texto']) ?>
                     </div>
                 </div>
             </div>
-            <script scr="lib/bootstrap-4.2.1-dist/js/bootstrap.min.js"></script>
-        </body>
-    </html>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <?php
+                    include 'includes/rodape.php';
+                ?>
+            </div>
+        </div>
+    </div>
+    <script src="lib/bootstrap-4.2.1-dist/js/bootstrap.min.js"></script>
+</body>
+</html>
