1d945714ccdbbfbb05a7f51d99d4fb01d99d11aa
oxeakah321 <ktoledopessoa@gmail.com>
2025-09-29 15:02:46 -0300

    finalissimos

---
+++ post_formulario.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Post | Projeto para WEB com PHP</title>
+        <link rel="stylesheet"
+              href="lib/bootstrap-4.2.1-dist/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <?php
+                    include 'includes/topo.php';
+                    include 'inludes/valida_login.php';
+                    ?>
+
+                </div>
+            </div>
+            <div class="row" style="min-height: 500px;">
+                <div class="col-md-12">
+                    <?php ?>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
